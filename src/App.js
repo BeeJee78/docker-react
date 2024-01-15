04bdc695288d6b4152242873563f437bc852d215
Bart Jongman <bartjongman@Barts-MBP.fritz.box>
2024-01-15 19:52:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hoi Bart! Hoe gaat het? Slecht? Mooi! Haha
         </p>
         <a
           className="App-link"
